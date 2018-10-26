0909c83668d68b7ebe7429427da1363150d9620d
sayed_zakrea <sayed_zakrea@hotmail.com>
2018-10-27 00:23:27 +0200

    xamarin forms overlay page
    How To Show A Loading Screen using xamarin forms

---
+++ overlayexample/overlayexample.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace overlayexample.Droid
+{
+    [Activity(Label = "overlayexample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ overlayexample/overlayexample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace overlayexample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Popupage page1 = new Popupage();
+            await PopupNavigation.Instance.PushAsync(page1);
+            await Task.Delay(5000);
+            MessagingCenter.Send<MainPage>(this, "Hi");
+        }
+    }
+}
